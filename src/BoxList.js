9f251c05cc01e33bcb2a924ce21706d4057a2c0f
RalphsCode <RalphsCode@gmail.com>
2024-12-17 16:57:43 -0800

    Functionality in place, needing a Box Component and form to add new box.

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,11 +1,30 @@
 import React, {useState} from 'react';
 
 const BoxList = () => {
-    const [allBoxes, setAllBoxes] = useState([{"color":"grey", "width": 100, "height": 100}]);
+    const [allBoxes, setAllBoxes] = useState([{ "id": 1, "color":"purple", "width": 100, "height": 100},
+        { "id": 2, "color":"red", "width": 50, "height": 50},
+        { "id": 3, "color":"yellow", "width": 150, "height": 150}
+    ]);
+
+    const removeItem = (x) => (
+        setAllBoxes(allBoxes.filter((box) => box.id !== x))
+    )
+
     return (
         <div>
-            <p> BoxList App </p>
-            <p style={{backgroundColor: allBoxes[0].color}}> allBoxes, color: {allBoxes[0].color} </p>
+            <p> BoxList App </p> 
+            { allBoxes.map( (box) => (
+                <p style={{
+                    backgroundColor: box.color, 
+                    height: `${box.height}px`, 
+                    width: `${box.width}px`, 
+                    margin: '5px',
+                    }}>Box {box.id} &nbsp;
+                    <button onClick={() => removeItem(box.id)}>X</button>
+                </p> 
+            ))}
+            
+            
         </div>
     ) 
 }
